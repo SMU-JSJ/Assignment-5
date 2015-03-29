7e8635f04cce895cd4c0e2ce417227d080162bd7
Jordan Kayse <jkayse@smu.edu>
2015-03-29 14:27:00 -0500

    Initial Commit

---
+++ LightMusic/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LightMusic
+//
+//  Created by ch484-mac7 on 3/29/15.
+//  Copyright (c) 2015 SMU. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LightMusic/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LightMusic
+//
+//  Created by ch484-mac7 on 3/29/15.
+//  Copyright (c) 2015 SMU. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
