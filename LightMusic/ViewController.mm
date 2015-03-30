8a12e5aa5deef86a322e9253118aa13f190ed5c0
Jordan Kayse <jkayse@smu.edu>
2015-03-29 19:31:15 -0500

    Added code to cycle through different songs for relax, party, and game arrays.

--- LightMusic/ViewController.mm
+++ LightMusic/ViewController.mm
@@ -9,7 +9,9 @@
 #import "ViewController.h"
 #import "Novocaine.h"
 #import "AudioFileReader.h"
-#import "RingBuffer.h"
+#import <MediaPlayer/MediaPlayer.h>
+
+#define kBufferLength 4096
 
 @interface ViewController ()
 
@@ -26,13 +28,43 @@
 @property (weak, nonatomic) IBOutlet UISlider *volumeSlider;
 @property (weak, nonatomic) IBOutlet UIButton *playPauseButton;
 
-@property (strong, nonatomic) NSArray* descriptionArray;
+@property (strong, nonatomic) Novocaine *audioManager;
+@property (nonatomic) float *audioData;
+
+@property (strong, nonatomic) NSTimer *timer;
+
+@property (strong, nonatomic) NSArray *descriptionArray;
+@property (strong, nonatomic) NSArray *relaxSongs;
+@property (strong, nonatomic) NSArray *partySongs;
+@property (strong, nonatomic) NSArray *gameSongs;
+@property (strong, nonatomic) NSDictionary *songTimes;
+@property (strong, nonatomic) NSDictionary *songArtists;
 
 @property (nonatomic) MusicMode mode;
+@property (nonatomic) BOOL playing;
+@property (nonatomic) int songIndex;
 
 @end
 
 @implementation ViewController
+
+AudioFileReader *fileReader;
+
+// Lazily instantiate
+
+- (Novocaine*)audioManager {
+    if (!_audioManager) {
+        _audioManager = [Novocaine audioManager];
+    }
+    return _audioManager;
+}
+
+- (float*)audioData {
+    if (!_audioData) {
+        _audioData = (float*)calloc(kBufferLength, sizeof(float));
+    }
+    return _audioData;
+}
 
 - (NSArray *)descriptionArray {
     if (!_descriptionArray) {
@@ -46,9 +78,90 @@
     return _descriptionArray;
 }
 
+- (NSArray *)relaxSongs {
+    if (!_relaxSongs) {
+        _relaxSongs = [NSArray arrayWithObjects:@"Sweet Caroline", @"Keep Holding On", @"Seasons Of Love", nil];
+    }
+    
+    return _relaxSongs;
+}
+
+- (NSArray *)partySongs {
+    if (!_partySongs) {
+        _partySongs = [NSArray arrayWithObjects:@"All About That Bass", @"Uptown Funk", @"Jump", nil];
+    }
+    
+    return _partySongs;
+}
+
+- (NSArray *)gameSongs {
+    if (!_gameSongs) {
+        _gameSongs = [NSArray arrayWithObjects:@"Freeze Frame", nil];
+    }
+    
+    return _gameSongs;
+}
+
+- (NSDictionary *)songArtists {
+    if (!_songArtists) {
+        _songArtists = @{
+                       @"Jump" : @"The Glee Cast", @"Uptown Funk" : @"The Glee Cast",
+                       @"All About That Bass" : @"The Glee Cast", @"Sweet Caroline" : @"The Glee Cast",
+                       @"Keep Holding On" : @"The Glee Cast", @"Seasons of Love" : @"The Glee Cast",
+                       @"Freeze Frame" : @"The J. Geils Band"
+                       };
+    }
+    
+    return _songArtists;
+}
+
+- (NSDictionary *)songTimes {
+    if (!_songTimes) {
+        _songTimes = @{
+                       @"Jump" : @236, @"Uptown Funk" : @262,
+                       @"All About That Bass" : @189, @"Sweet Caroline" : @119,
+                       @"Keep Holding On" : @245, @"Seasons of Love" : @185,
+                       @"Freeze Frame" : @237
+        };
+    }
+    
+    return _songTimes;
+}
+
 - (void)setMode:(MusicMode)mode {
     _mode = mode;
     [self updateMode];
+    [self setAudioOutput];
+    [self createTimer];
+}
+
+- (void)setPlaying:(BOOL)playing {
+    _playing = playing;
+    if (playing) {
+        [self.playPauseButton setImage:[UIImage imageNamed:@"pause.png"]
+                              forState:UIControlStateNormal];
+        [fileReader play];
+        if (![self.audioManager playing])
+            [self.audioManager play];
+    } else {
+        [self.playPauseButton setImage:[UIImage imageNamed:@"play.png"]
+                              forState:UIControlStateNormal];
+        [fileReader pause];
+        if ([self.audioManager playing])
+            [self.audioManager pause];
+    }
+}
+
+- (void)setSongIndex:(int)songIndex {
+    if (self.mode == RELAX) {
+        songIndex = songIndex % [self.relaxSongs count];
+    } else if (self.mode == PARTY) {
+        songIndex = songIndex % [self.partySongs count];
+    } else if (self.mode == GAME) {
+        songIndex = songIndex % [self.gameSongs count];
+    }
+    
+    _songIndex = songIndex;
 }
 
 - (void)viewDidLoad {
@@ -56,12 +169,97 @@
     // Do any additional setup after loading the view, typically from a nib.
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+-(void)viewWillAppear:(BOOL)animated{
+    [super viewWillAppear:animated];
+    
+    self.volumeSlider.value = 1.0;
+    
+    [self setAudioOutput];
+    [self createTimer];
+    
+    self.playing = YES;
+    // handle what happens if the view gets loaded again
+//    if(![self.audioManager playing])
+//        [self.audioManager play];
+}
+
+- (void)viewWillDisappear:(BOOL)animated{
+    [super viewWillDisappear:animated];
+    
+    [self.audioManager pause]; // just pause the playing
+    // audio manager is a singleton class, so we do not need to
+    // tear it down, in case some other controller may want to use it
+    
+    _audioManager = nil;
+    
+    [self.timer invalidate];
+}
+
+- (void)createTimer {
+    NSLog(@"create timer");
+    
+    if ([self.timer isValid]) {
+        NSLog(@"invalidate timer");
+        [self.timer invalidate];
+    }
+    
+    NSString* songName = [self getSongAtIndex:self.songIndex];
+    NSNumber* timerNum = (NSNumber *)[self.songTimes valueForKey:songName];
+    NSInteger timerInt = [timerNum integerValue];
+    
+    NSLog(@"%ld", timerInt);
+    
+    self.timer = [NSTimer scheduledTimerWithTimeInterval:timerInt
+                                                  target:self
+                                                selector:@selector(setAudioOutput)
+                                                userInfo:nil
+                                                 repeats:NO];
+    
+    [[NSRunLoop mainRunLoop] addTimer:self.timer forMode:NSRunLoopCommonModes];
+    
+    self.songIndex = self.songIndex + 1;
+}
+
+- (void)setAudioOutput {
+    NSString* songName = [self getSongAtIndex:self.songIndex];
+    self.songTitleLabel.text = songName;
+    self.artistLabel.text = (NSString *)[self.songArtists valueForKey:songName];
+    
+    NSLog(@"Song index = %d\nSong name = %@", self.songIndex, songName);
+    // load samples from an mp3 file and set them to output to the speakers!!
+    NSURL *inputFileURL = [[NSBundle mainBundle] URLForResource:songName
+                                                  withExtension:@"mp3"];
+    
+    fileReader = [[AudioFileReader alloc]
+                  initWithAudioFileURL: inputFileURL
+                  samplingRate: self.audioManager.samplingRate
+                  numChannels: self.audioManager.numOutputChannels];
+    
+    
+    //[fileReader play];
+    fileReader.currentTime = 0.0;
+    
+    [self.audioManager setOutputBlock:^(float *data, UInt32 numFrames, UInt32 numChannels)
+     {
+         // this loads new samples from the file reader object and saves them into the output speaker buffers
+         [fileReader retrieveFreshAudio:data numFrames:numFrames numChannels:numChannels];
+         
+     }];
+}
+
+- (NSString *)getSongAtIndex:(int)songIndex {
+    if (self.mode == RELAX) {
+        return self.relaxSongs[songIndex];
+    } else if (self.mode == PARTY) {
+        return self.partySongs[songIndex];
+    } else {
+        return self.gameSongs[songIndex];
+    }
 }
 
 - (void)updateMode {
+    self.songIndex = 0;
+    
     UIImage *image1, *image2, *image3;
     UIColor *color1, *color2, *color3;
     
@@ -103,33 +301,28 @@
 }
 
 - (IBAction)volumeSliderChanged:(UISlider *)sender {
+    MPMusicPlayerController *musicPlayer = [MPMusicPlayerController applicationMusicPlayer];
+    musicPlayer.volume = sender.value;
 }
 
-- (IBAction)relaxButtonClicked:(UIButton *)sender {
+- (IBAction)relaxClicked:(UIButton *)sender {
     self.mode = RELAX;
 }
 
-- (IBAction)relaxLabelClicked:(UIButton *)sender {
-    self.mode = RELAX;
-}
-
-- (IBAction)partyButtonClicked:(UIButton *)sender {
+- (IBAction)partyClicked:(UIButton *)sender {
     self.mode = PARTY;
 }
 
-- (IBAction)partyLabelClicked:(UIButton *)sender {
-    self.mode = PARTY;
-}
-
-- (IBAction)gameButtonClicked:(UIButton *)sender {
-    self.mode = GAME;
-}
-
-- (IBAction)gameLabelClicked:(UIButton *)sender {
+- (IBAction)gameClicked:(UIButton *)sender {
     self.mode = GAME;
 }
 
 - (IBAction)playPauseButtonClicked:(UIButton *)sender {
+    if (self.playing) {
+        self.playing = NO;
+    } else {
+        self.playing = YES;
+    }
 }
 
 @end
