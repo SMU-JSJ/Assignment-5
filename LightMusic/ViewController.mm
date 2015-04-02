5ee4061b429df395f838820b538d40b87c843fb2
Jordan Kayse <jkayse@smu.edu>
2015-04-01 20:55:16 -0500

    fixed bugs with BLE communication

--- LightMusic/ViewController.mm
+++ LightMusic/ViewController.mm
@@ -59,7 +59,7 @@ AudioFileReader *fileReader;
 
 // Lazily instantiate
 
-- (SongModel*) songModel {
+- (SongModel*)songModel {
     if(!_songModel)
         _songModel = [SongModel sharedInstance];
     
@@ -101,24 +101,90 @@ AudioFileReader *fileReader;
     _mode = mode;
     [self updateMode];
     [self setAudioOutput];
+    self.playing = self.playing;
+}
+
+- (void)updateMode {
+    self.songIndex = 0;
+    
+    UIImage *image1, *image2, *image3;
+    UIColor *color1, *color2, *color3;
+    
+    image1 = [UIImage imageNamed:@"relax_grey.png"];
+    image2 = [UIImage imageNamed:@"party_grey.png"];
+    image3 = [UIImage imageNamed:@"game_grey.png"];
+    
+    color1 = [UIColor darkGrayColor];
+    color2 = [UIColor darkGrayColor];
+    color3 = [UIColor darkGrayColor];
+    
+    if (self.mode == RELAX) {
+        image1 = [UIImage imageNamed:@"relax_blue.png"];
+        color1 = [UIColor blueColor];
+    } else if (self.mode == PARTY) {
+        image2 = [UIImage imageNamed:@"party_blue.png"];
+        color2 = [UIColor blueColor];
+    } else if (self.mode == GAME) {
+        image3 = [UIImage imageNamed:@"game_blue.png"];
+        color3 = [UIColor blueColor];
+    }
+    
+    self.descriptionLabel.text = self.descriptionArray[self.mode];
+    
+    // Set image and color for the relax button and label
+    [self.relaxButton setImage:image1 forState:UIControlStateNormal];
+    [self.relaxLabel setTitleColor:color1
+                          forState:UIControlStateNormal];
+    
+    // Set image and color for the party button and label
+    [self.partyButton setImage:image2 forState:UIControlStateNormal];
+    [self.partyLabel setTitleColor:color2
+                          forState:UIControlStateNormal];
+    
+    // Set image and color for the game button label
+    [self.gameButton setImage:image3 forState:UIControlStateNormal];
+    [self.gameLabel setTitleColor:color3
+                         forState:UIControlStateNormal];
 }
 
 - (void)setPlaying:(BOOL)playing {
+    
     _playing = playing;
+    
     if (playing) {
-        [self.playPauseButton setImage:[UIImage imageNamed:@"pause.png"]
-                              forState:UIControlStateNormal];
+        if (self.mode == GAME && self.connected == CONNECTED) {
+            [self.playPauseButton setImage:[UIImage imageNamed:@"pause_disabled.png"]
+                                  forState:UIControlStateNormal];
+            [self.playPauseButton setEnabled:NO];
+        } else {
+            [self.playPauseButton setImage:[UIImage imageNamed:@"pause.png"]
+                                  forState:UIControlStateNormal];
+            [self.playPauseButton setEnabled:YES];
+        }
+        
         [fileReader play];
         if (![self.audioManager playing])
             [self.audioManager play];
         [self createTimer];
     } else {
-        [self.playPauseButton setImage:[UIImage imageNamed:@"play.png"]
-                              forState:UIControlStateNormal];
+        if (self.mode == GAME && self.connected == CONNECTED) {
+            [self.playPauseButton setImage:[UIImage imageNamed:@"play_disabled.png"]
+                                  forState:UIControlStateNormal];
+            [self.playPauseButton setEnabled:NO];
+        } else {
+            [self.playPauseButton setImage:[UIImage imageNamed:@"play.png"]
+                                  forState:UIControlStateNormal];
+            [self.playPauseButton setEnabled:YES];
+        }
+        
         [fileReader pause];
         if ([self.audioManager playing])
             [self.audioManager pause];
         [self.songTimeDecrementer invalidate];
+    }
+    
+    if (self.connected == CONNECTED) {
+        [self sendDataToArduino];
     }
 }
 
@@ -129,12 +195,11 @@ AudioFileReader *fileReader;
                                       forState:UIControlStateNormal];
     } else if (connected == CONNECTING) {
         [self.connectDisconnectButton setImage:[UIImage imageNamed:@"connecting.png"]
-                              forState:UIControlStateNormal];
-        
+                                      forState:UIControlStateNormal];
         [self scanForDevices];
     } else if (connected == CONNECTED) {
         [self.connectDisconnectButton setImage:[UIImage imageNamed:@"connected.png"]
-                              forState:UIControlStateNormal];
+                                      forState:UIControlStateNormal];
     }
 }
 
@@ -166,7 +231,7 @@ AudioFileReader *fileReader;
     [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector (OnBLEDidReceiveData:) name:@"BLEReceievedData" object:nil];
 }
 
--(void)viewWillAppear:(BOOL)animated{
+- (void)viewWillAppear:(BOOL)animated {
     [super viewWillAppear:animated];
     
     self.volumeSlider.value = 1.0;
@@ -175,12 +240,9 @@ AudioFileReader *fileReader;
     [self createTimer];
     
     self.playing = YES;
-    // handle what happens if the view gets loaded again
-//    if(![self.audioManager playing])
-//        [self.audioManager play];
 }
 
-- (void)viewWillDisappear:(BOOL)animated{
+- (void)viewWillDisappear:(BOOL)animated {
     [super viewWillDisappear:animated];
     
     [self.audioManager pause]; // just pause the playing
@@ -192,6 +254,35 @@ AudioFileReader *fileReader;
     [self.songTimeDecrementer invalidate];
     
     // call function to disconnect BLE
+}
+
+
+- (void)sendDataToArduino {
+    int LEDcode;
+    
+    // Concactenates the LED color codes and mode code together
+    // The byte is for the red LED, second byte is for the
+    // green LED, and last byte is for the mode
+    // 0x11 - red on,    0x10 - red off
+    // 0x21 - green on,  0x20 - green off
+    // 0x00 - not party, 0x01 - party
+    if (self.playing) {
+        if (self.mode != PARTY) {
+            LEDcode = 0x102100;
+        } else {
+            LEDcode = 0x102101;
+        }
+    } else {
+        if (self.mode != PARTY) {
+            LEDcode = 0x112000;
+        } else {
+            LEDcode = 0x112001;
+        }
+    }
+    
+    NSData *data = [NSData dataWithBytes: &LEDcode length: sizeof(LEDcode)];
+    
+    [self.bleShield write:data];
 }
 
 - (void)scanForDevices {
@@ -264,7 +355,6 @@ AudioFileReader *fileReader;
     self.songTitleLabel.text = songName;
     self.artistLabel.text = (NSString *)[self.songModel.songArtists valueForKey:songName];
     
-    NSLog(@"Song index = %d\nSong name = %@", self.songIndex, songName);
     // load samples from an mp3 file and set them to output to the speakers!!
     NSURL *inputFileURL = [[NSBundle mainBundle] URLForResource:songName
                                                   withExtension:@"mp3"];
@@ -294,47 +384,9 @@ AudioFileReader *fileReader;
     }
 }
 
-- (void)updateMode {
-    self.songIndex = 0;
-    
-    UIImage *image1, *image2, *image3;
-    UIColor *color1, *color2, *color3;
-    
-    image1 = [UIImage imageNamed:@"relax_grey.png"];
-    image2 = [UIImage imageNamed:@"party_grey.png"];
-    image3 = [UIImage imageNamed:@"game_grey.png"];
-    
-    color1 = [UIColor darkGrayColor];
-    color2 = [UIColor darkGrayColor];
-    color3 = [UIColor darkGrayColor];
-    
-    if (self.mode == RELAX) {
-        image1 = [UIImage imageNamed:@"relax_blue.png"];
-        color1 = [UIColor blueColor];
-    } else if (self.mode == PARTY) {
-        image2 = [UIImage imageNamed:@"party_blue.png"];
-        color2 = [UIColor blueColor];
-    } else if (self.mode == GAME) {
-        image3 = [UIImage imageNamed:@"game_blue.png"];
-        color3 = [UIColor blueColor];
-    }
-    
-    self.descriptionLabel.text = self.descriptionArray[self.mode];
-    
-    // Set image and color for the relax button and label
-    [self.relaxButton setImage:image1 forState:UIControlStateNormal];
-    [self.relaxLabel setTitleColor:color1
-                 forState:UIControlStateNormal];
-    
-    // Set image and color for the party button and label
-    [self.partyButton setImage:image2 forState:UIControlStateNormal];
-    [self.partyLabel setTitleColor:color2
-                          forState:UIControlStateNormal];
-    
-    // Set image and color for the game button label
-    [self.gameButton setImage:image3 forState:UIControlStateNormal];
-    [self.gameLabel setTitleColor:color3
-                         forState:UIControlStateNormal];
+- (void)dealloc {
+    free(self.audioData);
+    self.audioManager = nil;
 }
 
 - (IBAction)volumeSliderChanged:(UISlider *)sender {
@@ -355,13 +407,16 @@ AudioFileReader *fileReader;
 }
 
 - (IBAction)playPauseButtonClicked:(UIButton *)sender {
+    [self togglePlayPause];
+}
+
+- (void)togglePlayPause {
     if (self.playing) {
         self.playing = NO;
     } else {
         self.playing = YES;
     }
 }
-
 
 - (IBAction)connectDisconnectButtonClicked:(UIButton *)sender {
     if (self.connected) {
@@ -375,52 +430,93 @@ AudioFileReader *fileReader;
 }
 
 #pragma mark - BLEdelegate protocol methods
-// NEW FUNCTION EXAMPLE: parse the received data from NSNotification
+// Parse the received data from NSNotification
 -(void) OnBLEDidReceiveData:(NSNotification *)notification
 {
-    NSData* d = [[notification userInfo] objectForKey:@"data"];
-    NSLog(@"%@", [d description]);
-    //    NSUInteger decodedInteger;
-    //    [d getBytes:&decodedInteger length:sizeof(decodedInteger)];
-    //    NSLog(@"Data: %lu", (unsigned long)decodedInteger);
+    NSMutableData* d = [[notification userInfo] objectForKey:@"data"];
+    const uint8_t *bytes = (const uint8_t *)[d bytes];
     
-    //NSString *s = [[NSString alloc] initWithData:d encoding:NSUTF8StringEncoding];
-    //self.label.text = s;
-    //NSLog(@"%@", s);
+    uint8_t B0 = bytes[0], B3 = bytes[3], B1 = 0, P1 = 0, P2 = 0, L1 = 0, L2 = 0;
+    
+    // If the first byte is 0A and the fourth byte is 0B
+    //   You  will receive button data, potentiometer data, and light sensor data
+    //   (9 bytes total)
+    //   B0: 0A - indicating that the button values are next
+    //   B1 and B2: value of the button
+    //   B3: 0B - indicating that the potentiometer values are next
+    //   B4 and B5: value of the potentiometer
+    //   B6: 0B - indicating that the light sensor values are next
+    //   B7 and B8: value of the light sensor
+    // If the first byte is 0A and the fourth byte is 0C
+    //   You  will receive button data and light sensor data
+    //   (9 bytes total)
+    //   B0: 0A - indicating that the button values are next
+    //   B1 and B2: value of the button
+    //   B3: 0B - indicating that the light sensor values are next
+    //   B4 and B5: value of the light sensor
+    // If the first byte will be 0B
+    //   You will receive potentiometer data and light sensor data
+    //   (6 bytes total)
+    //   B0: 0B - indicating that the potentiometer values are next
+    //   B1 and B2: value of the potentiometer
+    //   B3: 0B - indicating that the light sensor values are next
+    //   B4 and B5: value of the light sensor
+    // Otherwise, the first byte will be 0C
+    //   You will receive light sensor data
+    //   (3 bytes total)
+    //   B0: 0B - indicating that the light values are next
+    //   B1 and B2: value of the light
+    if (B0 == 10 && B3 == 11) {
+        // There are 9 bytes
+        B1 = bytes[1], P1 = bytes[4], P2 = bytes[5], L1 = bytes[7], L2 = bytes[8];
+    } else if (B0 == 10 && B3 == 12) {
+        // There are 6 bytes
+        B1 = bytes[1], L1 = bytes[4], L2 = bytes[5];
+    } else if (B0 == 11) {
+        // There are 6 bytes
+        P1 = bytes[1], P2 = bytes[2], L1 = bytes[4], L2 = bytes[5];
+    } else {
+        // Ther are 3 bytes
+        L1 = bytes[1], L2 = bytes[2];
+    }
+    
+    // Button: if B1 is 1, toggle play/pause
+    if (B0 == 10 && B1 == 1) {
+        [self togglePlayPause];
+    }
+    
+    // Potentiometer
+    if (B0 == 11 || B3 == 11) {
+        int potentiometer = (P1 << 8) + P2;
+        float volumeValue = (float)potentiometer/1023.0;
+        if(volumeValue >= self.volumeSlider.value + 0.1 ||
+           volumeValue <= self.volumeSlider.value - 0.1) {
+            MPMusicPlayerController *musicPlayer = [MPMusicPlayerController applicationMusicPlayer];
+            musicPlayer.volume = volumeValue;
+            self.volumeSlider.value = volumeValue;
+        }
+    }
+    
+    // Light sensor
+    if (self.mode == GAME) {
+        int light = (L1 << 8) + L2;
+        if (light >= 900) {
+            self.playing = NO;
+        } else {
+            self.playing = YES;
+        }
+    }
 }
 
-// NEW FUNCTION: we disconnected, stop running
+// we disconnected, stop running
 - (void) OnBLEDidDisconnect:(NSNotification *)notification {
-    // disconnect
     self.connected = DISCONNECTED;
+    self.playing = self.playing;
 }
 
-//CHANGE 7: create function called from "BLEDidConnect" notification (you can change the function below)
-// in this function, update a label on the UI to have the name of the active peripheral
-// you might be interested in the following method:
-// NSString *deviceName =[notification.userInfo objectForKey:@"deviceName"];
-// now just wait to send or receive
 -(void) OnBLEDidConnect:(NSNotification *)notification {
     self.connected = CONNECTED;
+    self.playing = self.playing;
 }
-
-#pragma mark - UI operations storyboard
-- (IBAction)BLEShieldSend:(id)sender {
-    
-    //Note: this function only needs a name change, the BLE writing does not change
-    NSString *s;
-    NSData *d;
-    
-//    if (self.textField.text.length > 16)
-//        s = [self.textField.text substringToIndex:16];
-//    else
-//        s = self.textField.text;
-    
-    s = [NSString stringWithFormat:@"%@\r\n", s];
-    d = [s dataUsingEncoding:NSUTF8StringEncoding];
-    
-    [self.bleShield write:d];
-}
-
 
 @end
